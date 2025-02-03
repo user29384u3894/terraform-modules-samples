ced79b0d4c8363c85e7c51fe9fe0032eeaf2ecfa
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2025-02-02 19:59:16 -0500

    AppHub - type = "REGIONAL"

--- gcp-app-hub-app/main.tf
+++ gcp-app-hub-app/main.tf
@@ -2,7 +2,7 @@ resource "google_apphub_application" "apphub_app" {
   location       = var.region
   application_id = var.app_id
   scope {
-    type = "GLOBAL"
+    type = "REGIONAL"
   }
   attributes {
     environment {
